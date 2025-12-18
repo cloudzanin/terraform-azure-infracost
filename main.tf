7248ebbfb89c9d4a825bf8a1175014b05b1a812d
Sarah Lean <sarahlean@microsoft.com>
2025-12-18 14:07:46 +0000

    initial commit

---
+++ main.tf
@@ -0,0 +1,58 @@
+terraform {
+  required_version = ">= 1.9.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 4.0.0, < 5.0.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = ">= 3.5.0, < 4.0.0"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = ">= 4.0.0, < 5.0.0"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = ">= 2.4.0, < 3.0.0"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = ">= 3.2.0, < 4.0.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = var.azure_subscription_id
+}
+
+# This ensures we have unique CAF compliant names for our resources.
+module "naming" {
+  source  = "Azure/naming/azurerm"
+  version = "0.4.2"
+}
+
+locals {
+  azure_regions = [
+    "ukwest",
+    "westeurope",
+    "francecentral",
+    "swedencentral"
+    # Add other regions as needed
+  ]
+}
+
+# This picks a random region from the list of regions.
+resource "random_integer" "region_index" {
+  max = length(local.azure_regions) - 1
+  min = 0
+}
+
+# This is required for resource modules
+resource "azurerm_resource_group" "rg" {
+  location = local.azure_regions[random_integer.region_index.result]
+  name     = module.naming.resource_group.name_unique
+}
