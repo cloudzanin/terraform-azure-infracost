3494280469f54824f081927a08b1aa5bdb9afc5b
Sarah Lean <sarahlean@microsoft.com>
2025-12-18 15:28:10 +0000

    update app plan name

--- main.tf
+++ main.tf
@@ -58,7 +58,7 @@ resource "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_service_plan" "appplan" {
-  name                = "example"
+  name                = module.naming.service_plan.name_unique
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   os_type             = "Linux"
